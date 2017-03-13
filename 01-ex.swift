1f137fe934e5936e8aff681358c36fd2bdba1292
Petar Ivanov <pepincho95@gmail.com>
2017-03-13 19:45:01 +0200

    Update sumNumbersFromString func from exercise 1

--- 01-ex.swift
+++ 01-ex.swift
@@ -11,9 +11,15 @@ import Foundation
 
 func sumNumbersFromString(str: String) -> Int {
   let strArr = str.components(separatedBy: " ")
+
   let numbers = strArr.map { Int($0) ?? 0 }
+//  let numbers = strArr.flatMap { Int($0) }
+//  let numbers = strArr.map { Int($0) }
+//  let numbers = strArr.map { Int($0)!
+//  let numbers = strArr.map { Int($0)! ?? 0 }
 
   return numbers.reduce(0, +)
 }
 
+// print(sumNumbersFromString(str: "0 1 2 3dd 4 5 6 7 8 9 10")) // 52
 print(sumNumbersFromString(str: "0 1 2 3 4 5 6 7 8 9 10")) // 55
