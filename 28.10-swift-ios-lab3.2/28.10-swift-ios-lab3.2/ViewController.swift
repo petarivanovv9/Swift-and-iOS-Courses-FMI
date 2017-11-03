8c8c6893518e0b8b636ed2a666610fa9f6e30985
Petar Ivanov <pepincho95@gmail.com>
2017-11-03 14:54:41 +0200

    Add initial version of 28.10 lab - exercise 1

---
+++ 28.10-swift-ios-lab3.2/28.10-swift-ios-lab3.2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  28.10-swift-ios-lab3.2
+//
+//  Created by Petar Ivanov on 10/28/17.
+//  Copyright © 2017 Petar Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
